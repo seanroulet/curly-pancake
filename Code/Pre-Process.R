ab50af52f1bde85ad7647a904dc3d8806f737bbf
Sean Roulet <seanroulet@gmail.com>
2018-09-21 10:32:37 -0300

    Uploading basics

---
+++ Code/Pre-Process.R
@@ -0,0 +1,15 @@
+###### Pre-Process for cuESTARFM
+
+####INPUTS
+#### Landsat images in tar.gz format. for the corresponding dates
+#### MODIS images cropped by shapefile to all the dates
+
+##### Extract Landsat BANDS.
+##### Bands of interest are 
+##### L8  R<-Band 4  
+#####     NIR<- Band 5
+#####
+##### L7  R<-Band 3
+#####     NIR<- Band 4
+
+
