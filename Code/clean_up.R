a581e98fe16a8f2e9d97509b087dd3fc6bd9e8a7
Sean Roulet <seanroulet@gmail.com>
2018-10-05 18:55:58 -0300

    Initial script for cleanup post pre-processing

---
+++ Code/clean_up.R
@@ -0,0 +1,10 @@
+##### Cleanup of folders after ESTARFM 
+
+### not to be run yet.  needs to be tested.
+
+Folders_to_clean<-c("MODIS/MODIS_B1","MODIS/MODIS_B2","extracted","reprojected","tmp") # possibly "INDEX"
+for( i in 1:length(Folders_to_clean)){
+  message(paste("clean",Folders_to_clean[i]))
+  files_to_remove<-list.files(file.path(getwd(),"Rasters",Folders_to_clean[i]), full.names=TRUE)
+  message(files_to_remove)
+  }
