0647dbc05f64b23602a1e22d973cb0e471fd7e64
Sean Roulet <seanroulet@gmail.com>
2018-10-06 11:38:08 -0300

    Modified all functions so they work independently

--- Code/Pre-Process.R
+++ Code/Pre-Process.R
@@ -21,15 +21,28 @@ library(tools)
 
 ############ START Pre Processing ###########
 #### Set the working directory
-setwd("/Volumes/PLEXTOR/JASPR/cuESTARFM-R")
 
+create_Directories_for_cuESTARFM<-function(){
 
-#### Check if folders exist. Extracted, Processed, tmp etc.
+  #### Check if folders exist. Extracted, Processed, tmp etc.
+  # Landsat Directories
+  directoryExists("Rasters/LANDSAT/extracted")
+  directoryExists("Rasters/LANDSAT/LANDSAT_R")
+  directoryExists("Rasters/LANDSAT/LANDSAT_NIR")
+  directoryExists("Rasters/LANDSAT/CROPPED")
+  
+  # MODIS directories
+  directoryExists("Rasters/MODIS/REPROJECTED")
+  directoryExists("Rasters/MODIS/MODIS_B1")
+  directoryExists("Rasters/MODIS/MODIS_B2")
+  
+  directoryExists("Rasters/Ready_for_cuESTARFM")
+  directoryExists("Rasters/tmp")
+  directoryExists("Rasters/INDEX")
+  
+  
+}
 
-directoryExists("Rasters/extracted")
-directoryExists("Rasters/reprojected")
-directoryExists("Rasters/Ready_for_cuESTARFM")
-directoryExists("Rasters/tmp")
 
 ########  Set the tmp directory for the
 ########  Raster processes.  These take up 
@@ -39,71 +52,86 @@ directoryExists("Rasters/tmp")
 
 rasterOptions(tmpdir = "Rasters/tmp")
 
-extract_LANDSAT_for_cuESTARFM
-
-
-
-#################### Open a MODIS file
-### This needs to be modified to work though multiple directories.
-####################
-modisList<-list.files("Rasters/MODIS/MODIS_B2", full.names=TRUE, pattern=".*\\.tif$")
-
-modisSample<-raster(modisList[1])
-modisCRS<-crs(modisSample)
-modisExtent<-extent(modisSample)
-LandsatList<-list.files("Rasters/extracted", full.names=TRUE, pattern=".*\\.tif$")
-landsatSample<-raster(LandsatList[1])
-landsatCRS<-crs(landsatSample)
-#####################
-# i<-1
-for(i in 1:length(modisList)){
-  modisRaster<-raster(modisList[i])
-  #res(modisRaster30m)<-30
-  modisRasterUTM<-projectRaster(modisRaster,crs=landsatCRS)  # need to load the landsatCRS first!
-  modisRasterUTM30m<-disaggregate(modisRasterUTM, res(modisRaster)/30)  # closest number to 30
-  ### reproject again so that the pixels are exactly 30M.
-  modisRasterUTM30m<-projectRaster(modisRasterUTM30m, crs=landsatCRS, res=30)
-  # Save the new Raster
-  
-  filename<-paste("UTM",basename(modisList[i]),sep="_")
-  filepath<-file.path("Rasters/reprojected",filename)
-  modisUTMExtent<-extent(modisRasterUTM30m)
-  writeRaster(modisRasterUTM30m, filename = filepath)
-}
-
-
-
-
-
-landsatList<-list.files("Rasters/extracted",full.names=TRUE, pattern=".*\\.tif$")
-#i<-1
-for (i in 1:length(landsatList)){
-  landsatRaster<-raster(landsatList[i])
-  landsatCRS<-crs(landsatRaster)
-  landsatCrop<-crop(landsatRaster,modisRasterUTM30m, snap='near')
-  filename<-paste("UTM",basename(landsatList[i]),sep="_")
-  filepath<-file.path("Rasters/reprojected",filename)
-  
-  writeRaster(landsatCrop, filename = filepath)
-  
-  
-}
-
-
-
-
-
-
 #####################################################
 ##### FUNCTIONS SECTION #############################
 ##### Load before running code above ################
 #####################################################
 
-extract_LANDSAT_for_cuESTARFM<-function(){
+reproject_MODIS_to_Landsat<-function(MODIS_folder="Rasters/MODIS/MODIS_B1",Landsat_folder="Rasters/LANDSAT/LANDSAT_R"){
+  #################### Open a MODIS file
+  ### Loads a MODIS raster from download and reprojects it to a Landsat CRS
+  ####################
+  modisList<-list.files(MODIS_folder, full.names=TRUE, pattern=".*\\.tif$")
+  
+  # load a LANDSAT sample so we can get the crs, etc.
+  landsatList<-list.files(Landsat_folder, full.names=TRUE, pattern=".*\\.tif$")
+  landsatSample<-raster(landsatList[1])
+  landsatCRS<-crs(landsatSample)
+  
+  #####################
+  # i<-1
+  for(i in 1:length(modisList)){
+    modisRaster<-raster(modisList[i])
+
+    modisRasterUTM<-projectRaster(modisRaster,crs=landsatCRS)  
+    modisRasterUTM30m<-disaggregate(modisRasterUTM, res(modisRaster)/30)  # closest number to 30
+    
+    ### reproject again so that the pixels are exactly 30M.
+    modisRasterUTM30m<-projectRaster(modisRasterUTM30m, crs=landsatCRS, res=30)
+    
+    # Save the new Raster
+    filename<-basename(modisList[i])
+    filepath<-file.path("Rasters/MODIS/REPROJECTED",filename)
+    writeRaster(modisRasterUTM30m, filename = filepath)
+  }
+  
+  
+  
+}
+
+
+
+crop_LANDSAT_to_MODIS<-function(MODIS_folder="Rasters/MODIS/REPROJECTED",Landsat_folder="Rasters/LANDSAT/LANDSAT_R"){
+ 
+  ################
+  # Load a Landsat Folder and CROP it to the REPROJECTED MODIS file
+  ################
+  
+  # get a sample MODIS file so we can crop to it.
+  modisList<-list.files(MODIS_folder, full.names=TRUE, pattern=".*\\.tif$")
+  modisSample<-raster(modisList[1])
+  
+  landsatList<-list.files(Landsat_folder,full.names=TRUE, pattern=".*\\.tif$")
+  #i<-1
+  for (i in 1:length(landsatList)){
+    landsatRaster<-raster(landsatList[i])
+
+    landsatCrop<-crop(landsatRaster,modisSample, snap='near')
+    filename<-basename(landsatList[i])
+    filepath<-file.path("Rasters/CROPPED",filename)
+    
+    writeRaster(landsatCrop, filename = filepath)
+    
+    
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+extract_LANDSAT_for_cuESTARFM<-function(LANDSAT_folder="Rasters/LANDSAT"){
   # Extract bands for L7
   
   #### Get list of L7 files.
-  gzList<-list.files("Rasters/Landsat", full.names=TRUE, pattern="^LE07.*\\.gz")
+  gzList<-list.files(LANDSAT_folder, full.names=TRUE, pattern="^LE07.*\\.gz")
   lastRow<-length(gzList)
   
   ################################################
@@ -131,7 +159,7 @@ extract_LANDSAT_for_cuESTARFM<-function(){
     #### Extract Bands from L8
   
   #### Get list of L8 files.
-  gzList<-list.files("Rasters/Landsat", full.names=TRUE, pattern="^LC08.*\\.gz")
+  gzList<-list.files(LANDSAT_folder, full.names=TRUE, pattern="^LC08.*\\.gz")
   lastRow<-length(gzList)
   
   ################################################
@@ -158,27 +186,27 @@ extract_LANDSAT_for_cuESTARFM<-function(){
   
 }
 
-rename_LANDSAT_tif_R_NIR<-function(filePatterns,bands){
+rename_LANDSAT_tif_R_NIR<-function(filePatterns, bands, Extracted_folder="Rasters/LANDSAT/extracted"){
   ## rename the files to put the R or NIR
   for(j in 1:length(filePatterns)){
     
-    BandFiles<-list.files("Rasters/extracted",full.names=TRUE, pattern=filePatterns[j])
+    BandFiles<-list.files(Extracted_folder,full.names=TRUE, pattern=filePatterns[j])
     for(k in 1:length(BandFiles)){
       myExt<-file_ext(BandFiles[k])
       myNewFileName<-paste(file_path_sans_ext(BandFiles[k]),"_", bands[j],".",myExt, sep="")
+      myNewFileName<-paste("Rasters/LANDSAT/LANDSAT_",bands[j],"/",basename(myNewFilename),sep="")
       file.rename(BandFiles[k], myNewFileName)
     }
   }
   
 }
 
-extractBands<-function(zipFile,filePatterns){
+extractBands<-function(zipFile,filePatterns,extractDirectory="Rasters/LANDSAT/extracted"){
   # extract bands from landsat zipfile, and save into directory "extracted"
   # 
   bandFiles<-NULL
   
   # set the extract directory
-  extractDirectory <-file.path(getwd(),"Rasters/extracted")
   fileList<-untar(zipFile, list=TRUE)
   for(i in 1:length(filePatterns)){
     bandFiles[i]<-grep(filePatterns[i],fileList)
