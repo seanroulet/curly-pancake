6099ef26a21b0c98e71e37fd20f0907322cda291
Sean Roulet <seanroulet@gmail.com>
2018-10-09 12:42:45 -0300

    Added possibility of an unknown platfowm

--- Code/ifPlatform_F.R
+++ Code/ifPlatform_F.R
@@ -1,10 +1,19 @@
 ### function to check the OS and deliver different values depending on the answer
 
 ifPlatform<-function(ifWindows,ifUnix){
+
+  #### if UNIX
   if(.Platform$OS.type == "unix") {
     myAnswer <- ifUnix
-  } else {
+
+  ### if WINDOWS
+  } else if(.Platform$OS.type == "windows"){
     myAnswer <- ifWindows
+  
+  ### if unknown.
+  } else {
+    myAnswer <- "Error  Unkown Platform"
   }
   return(myAnswer)
 }
+
