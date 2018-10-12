8c748ec2ee94eb88fb3bc2768d6db6da91a7baed
mlcastellan <martin-castellan@hotmail.com>
2018-10-12 19:37:34 -0300

    Function to obtain shapefile centroid. To be used to ask Landsat for images.

--- Code/bb2cg.r
+++ Code/bb2cg.r
@@ -1,6 +1,7 @@
 ######## encuentro el centroide, ancho y alto del bbox de una finca######
 ######## esto se usara con la API de MODIS                       ########
 #########################################################################
+
 bb_input=file.choose()
 
 bb2cg=function(bb_input){
@@ -14,18 +15,23 @@ bb2cg=function(bb_input){
   #-------------------
   bb_01=readOGR(bb_input) 
   bb_01_crs=crs(bb_01)
-  #busco vertices del bbox
+  #-------------------
+  #el bbox tiene que estar como lat-lon
+  bb_01=spTransform(bb_01,CRS("+init=epsg:4326"))
+  bb_cg=gCentroid(bb_01)
+  #calculo los extremos del bbox
   lon1=bb_01@bbox[1,1]
   lon2=bb_01@bbox[1,2]
   lat1=bb_01@bbox[2,1]
   lat2=bb_01@bbox[2,2]
-  #calculo el alto y el ancho del bbox
-  alto=distm(c(lon1,lat1),c(lon1,lat2),fun=distHaversine)/1000
-  ancho=distm(c(lon1,lat1),c(lon2,lat1),fun=distHaversine)/1000
-  #calculo el centroide del bbox
-  bb_cg=gCentroid(bb_01)
-}
+  #-calculo con haversine las distancias
+  alto=distm(c(lon1,lat1),c(lon1,lat2), fun = distHaversine)
+  ancho=distm(c(lon1,lat1), c(lon2,lat1), fun = distHaversine)
+  
   
 
-#genero un get para traer el recorte en base al bbcx
-get_rquest=paste("https://modis.ornl.gov/rst/api/v1/MOD11A2/subset?latitude=",bb_cg@coords[2],"&longitude=",bb_cg@coords[1],"&band=LST_Day_1km&startDate=A2001001&endDate=A2001001&kmAboveBelow=",alto,"&kmLeftRight=",ancho,sep=""), add_headers(Accept = "text/csv"))
+  
+  
+  
+  
+}
